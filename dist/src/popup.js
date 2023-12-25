13e4fd7387cf133fd6221fd846ac86670893c2b6
Om Doiphode <omdoiphode161@gmail.com>
2023-12-26 00:29:26 +0530

    Renamed folders

--- dist/src/popup.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', async function() {
-    var myButton = document.getElementById('captureButton');
-    myButton.addEventListener('click', async() => {
-      console.log('Button clicked!');
-      const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-      chrome.tabs.sendMessage(tab.id, {type: 'init'}, (resp) => {
-      });
-    });
-  });
